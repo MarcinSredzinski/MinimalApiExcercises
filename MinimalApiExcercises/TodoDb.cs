3eb5e57e2a462492e03fed4bca01b9652c05bfc5
m.sredzinski94@gmail.com <m.sredzinski94@gmail.com>
2023-05-26 18:34:54 +0200

    Added model and db context class.

---
+++ MinimalApiExcercises/TodoDb.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalApiExcercises;
+
+public class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
+    public DbSet<ToDo> Todos => Set<ToDo>();
+}
