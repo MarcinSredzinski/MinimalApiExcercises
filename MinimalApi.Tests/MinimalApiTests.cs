aaf2acb46fc0264df97cc9bf8448b229b786de6a
m.sredzinski94 <m.sredzinski94@gmail.com>
2023-06-11 23:10:29 +0200

    Added tests project

---
+++ MinimalApi.Tests/MinimalApiTests.cs
@@ -0,0 +1,20 @@
+namespace MinimalApi.Tests
+{
+    public class MinimalApiTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetAllToDos_ShouldReturnOkOfTodosResult()
+        {           
+            //Arrange
+           // var db = CreateDbContext()
+            //Act 
+
+            //Assert
+        }
+    }
+}
